40e738f4c82b480bf2f6fd312e089303b7fad470
Omar20Ashraf <omaarashraf2020@gmail.com>
2023-01-15 20:23:30 +0200

    709. To Lower Case

---
+++ 709_to_lower_case.php
@@ -0,0 +1,16 @@
+<?php
+
+//php.test/leetCode/14_longest_common_prefix.php
+
+class Solution
+{
+
+    /**
+     * @param String $s
+     * @return String
+     */
+    function toLowerCase($s)
+    {
+        return strtolower($s);
+    }
+}
