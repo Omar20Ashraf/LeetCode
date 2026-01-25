41e386c29c56cd3c0ca972f2acdc3545ac5548be
Omar20Ashraf <o.ashraf@solutionplus.net>
2026-01-25 14:11:05 +0200

    wip: refactor MergeStringsAlternately

--- 1768-MergeStringsAlternately.php
+++ 1768-MergeStringsAlternately.php
@@ -1,14 +1,12 @@
 <?php
 
-//php.test/leetCode/1768_merge_strings_alternately.php
-
-class Solution
+class MergeStringsAlternately
 {
     function mergeAlternately(string $word1, string $word2): string
     {
         $len1 = strlen($word1);
         $len2 = strlen($word2);
-        $max  = max($len1, $len2);
+        $max = max($len1, $len2);
 
         $result = '';
         for ($i = 0; $i < $max; $i++) {
